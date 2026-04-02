14204065ab00921841a9fe7b0bec522c7627873b
TymurKashchuk <t1mu4337@gmail.com>
2026-04-02 17:21:29 +0300

    Observer pattern for LightHTML

---
+++ Observer/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Observer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var button = new LightElementNode("button", "inline", false);
+            button.AddChild(new LightTextNode("Click me"));
+
+            var clickListener = new EventListener("Listener1");
+            var hoverListener = new EventListener("Listener2");
+
+            button.Subscribe("click", clickListener);
+            button.Subscribe("mouseover", hoverListener);
+
+            Console.WriteLine(button.OuterHTML());
+
+            Console.WriteLine("\nTrigger click:");
+            button.TriggerEvent("click");
+
+            Console.WriteLine("\nTrigger mouseover:");
+            button.TriggerEvent("mouseover");
+        }
+    }
+}
